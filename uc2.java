3a78b2df3bcb596aa0460c5b9aac133b554db590
Snehal Das <niveditaguha78@gmail.com>
2026-03-02 23:45:35 +0530

    Add files via upload

---
+++ uc2.java
@@ -0,0 +1,22 @@
+/**
+ * OOPSBannerApp
+ *
+ * UC2: Print "OOPS" as a large ASCII banner using
+ *      individual print statements.
+ *
+ * @author Snehal Das
+ * @version 2.0
+ */
+public class uc2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****    *****    ***** ");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *   *****    ***** ");
+        System.out.println("*     *  *     *        *         *");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println(" *****    *****    *****    ***** ");
+    }
+}
