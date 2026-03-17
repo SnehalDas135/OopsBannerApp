6e9c4d904631cd297d224959ff27415040a71807
Snehal Das <niveditaguha78@gmail.com>
2026-03-17 08:22:02 +0530

    Add files via upload

---
+++ uc1.java
@@ -0,0 +1,22 @@
+/**
+ * OOPSBannerApp
+ * 
+ * UC1: Print the literal text "OOPS" to the console.
+ * 
+ * This is the entry-level implementation of the OOPS Banner Application.
+ * 
+ * @author Snehal Das
+ * @version 1.0
+ */
+public class uc1 {
+
+    /**
+     * Main method - entry point of the application.
+     * Prints the literal string "OOPS" to the console.
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
